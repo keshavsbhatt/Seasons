c30191bdf5e2b30a9277f88ab4161f6f51d1534f
keshavsbhatt <67513140+keshavsbhatt@users.noreply.github.com>
2020-07-18 20:05:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function copy() {
+    alert('No Copying :)');
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("yaay!Its Summer!");
+
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hurray! Its Winter");
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Woo-Hoo Its Autumn");
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("WOW! Its spring!");
+}
